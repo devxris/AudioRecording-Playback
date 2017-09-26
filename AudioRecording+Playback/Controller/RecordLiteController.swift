893a7f72cd5e7d7353ddfcb6beb15e4421a4f235
devxris <chuwei0915@gmail.com>
2017-09-26 13:38:37 +0800

    Simple UI and Setup

---
+++ AudioRecording+Playback/Controller/RecordLiteController.swift
@@ -0,0 +1,16 @@
+//
+//  RecordLiteController.swift
+//  AudioRecording+Playback
+//
+//  Created by Chris Huang on 26/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class RecordLiteController: UIViewController {
+
+
+
+}
+
